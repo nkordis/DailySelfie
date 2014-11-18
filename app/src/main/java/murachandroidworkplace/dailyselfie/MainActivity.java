c3f78711d241a4324050e73663eaa2f9ef92740b
Nikos Kordis <nikoskordis@gmail.com>
2014-11-18 23:09:16 +0100

    Take a selfie using the camera app.

--- app/src/main/java/murachandroidworkplace/dailyselfie/MainActivity.java
+++ app/src/main/java/murachandroidworkplace/dailyselfie/MainActivity.java
@@ -1,17 +1,30 @@
 package murachandroidworkplace.dailyselfie;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 
 public class MainActivity extends Activity {
+
+    private static final String TAG = "Daily Selfie";
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageView = (ImageView)findViewById(R.id.imageView);
     }
 
 
@@ -31,9 +44,28 @@ public class MainActivity extends Activity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.camera_icon) {
+            Log.i(TAG, "camera's icon pressed");
+            dispatchTakePictureIntent();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+
+            Log.i(TAG, "Image data returned");
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
